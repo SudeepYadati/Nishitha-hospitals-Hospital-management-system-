9fbde753d9efd71849dfc662adcd2d8b97f8e03c
SudeepYadati <Yadati.Sudeep@cognizant.com>
2025-03-31 00:43:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,255 @@
+﻿
+using HospitalManagementSystem.Data;
+using HospitalManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//  Database Connection
+builder.Services.AddDbContext<HospitalDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//  Identity Services (User Authentication)
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddIdentity<ApplicationUserModel, IdentityRole>()
+    .AddEntityFrameworkStores<HospitalDbContext>()
+    .AddDefaultTokenProviders();
+
+//  Identity Configuration
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;  
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = true;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(20);
+    options.Lockout.MaxFailedAccessAttempts = 12;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings
+    options.User.RequireUniqueEmail = true;
+});
+
+//  Authentication & Authorization
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+//  Adds support for Controllers with Views to container
+builder.Services.AddControllersWithViews();
+
+//Authorization policies for creating Admin and Patient users roles
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireClaim(ClaimTypes.Role, "Admin"));
+    options.AddPolicy("Patient", policy => policy.RequireClaim(ClaimTypes.Role, "Patient"));
+});
+// Builds web application
+var app = builder.Build();
+//  Configure Middleware components
+if (!app.Environment.IsDevelopment())  
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+//  Default Route
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+//  Role Seeding (Admin & Patient)
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    await CreateRoles(roleManager);
+}
+
+
+//  Create Default Roles (Admin, Patient)
+async Task CreateRoles(RoleManager<IdentityRole> roleManager)
+{
+    string[] roleNames = { "Admin", "Patient" };  
+    foreach (var roleName in roleNames)
+    {
+        var roleExists = await roleManager.RoleExistsAsync(roleName);
+        if (!roleExists)
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
+// Runs the web application
+app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using HospitalManagementSystem.Data;
+//using HospitalManagementSystem.Models;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+
+//var builder = WebApplication.CreateBuilder(args);
+////Database Connection
+//builder.Services.AddDbContext<HospitalDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddIdentity<ApplicationUserModel, IdentityRole>()
+//    .AddEntityFrameworkStores<HospitalDbContext>()
+//    .AddDefaultTokenProviders();
+////Identity Configuration 
+//builder.Services.Configure<IdentityOptions>(options =>
+//{
+//    //Password settings
+//    options.Password.RequireDigit = true;
+//    options.Password.RequiredLength = 5;
+//    options.Password.RequireNonAlphanumeric = false;
+//    options.Password.RequireUppercase = true;
+//    options.Password.RequireLowercase = false;
+//    //Lockout settings
+//    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+//    options.Lockout.MaxFailedAccessAttempts = 10;
+//    options.Lockout.AllowedForNewUsers = true;
+//    // User settings
+//    options.User.RequireUniqueEmail = true;
+//});
+
+//builder.Services.AddAuthentication();
+//builder.Services.AddAuthorization();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseRouting();
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+//using (var scope = app.Services.CreateScope())
+//{
+//var services = scope.ServiceProvider;
+//var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+//await CreateRoles(roleManager);
+//}
+
+//app.Run();
+
+//async Task CreateRoles(RoleManager<IdentityRole> roleManager)
+//{
+//    string[] roleNames = { "Admin", "Doctor", "Patient" };
+//    foreach (var roleName in roleNames)
+//    {
+//        var roleExist = await roleManager.RoleExistsAsync(roleName);
+//        if (!roleExist)
+//        {
+//            await roleManager.CreateAsync(new IdentityRole(roleName));
+//        }
+//    }
+//}
+
+//    await app.RunAsync();
